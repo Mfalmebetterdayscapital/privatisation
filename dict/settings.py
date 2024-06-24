7320636114b53b2108e55e07c77dc3d10bf3f780
willymwaura <wilsonmwaura697@gmail.com>
2024-06-24 03:47:49 +0300

    final changes

--- dict/settings.py
+++ dict/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'django-insecure-63q9h(znz#1e)m&74w9#_n5nakdb!qx*fqhfi=&k7e3bwmaia$
 # SECURITY WARNING: don't run with debug turned on in production!
 CORS_ORIGIN_ALLOW_ALL=True
 
-ALLOWED_HOSTS = ['privatisation-production.up.railway.app','kenyathena.up.railway.app','127.0.0.1']
-CSRF_TRUSTED_ORIGINS=['https://privatisation-production.up.railway.app','https://kenyathena.up.railway.app','https://127.0.0.1']
+ALLOWED_HOSTS = ['privatisation-production-a5e7.up.railway.app','127.0.0.1']
+CSRF_TRUSTED_ORIGINS=['https://privatisation-production-a5e7.up.railway.app','https://127.0.0.1']
 
 
 DEBUG=True
